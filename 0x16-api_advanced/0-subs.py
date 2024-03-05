ea80136746ce9f1df4e80a1d82bdbc27fa6c2348
Brokeice123 <keithmarkomen@gmail.com>
2024-03-05 11:46:15 +0000

    a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit.

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Function to query the Reddit API and return the number of subscribers
+for a given subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Gets the number of subscribers for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    # Send a request to the Reddit API
+    headers = {'User-Agent': 'Reddit API Client'}
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        nb_subscribers = data['data']['subscribers']
+        return (nb_subscribers)
+    else:
+        return (0)
